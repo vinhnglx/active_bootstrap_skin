de6c7f29625d1c54d20232ee66a97959febe5a4b
mherold <herold@emjot.de>
2018-07-16 15:14:07 +0200

    Precompile themify font (should fix 404 errors)

--- lib/active_bootstrap_skin.rb
+++ lib/active_bootstrap_skin.rb
@@ -3,6 +3,10 @@ require "active_bootstrap_skin/version"
 module ActiveBootstrapSkin
   module Rails
     class Engine < ::Rails::Engine
+      config.assets.precompile << '*.svg'
+      config.assets.precompile << '*.eot'
+      config.assets.precompile << '*.ttf'
+      config.assets.precompile << '*.woff'
     end
   end
 end
